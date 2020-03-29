ee835f568b994dfd6e50a46d2f346cee7b735c60
mikeharbidge <55110294+mikeharbidge@users.noreply.github.com>
2020-03-29 14:18:50 -0600

    Work on db_load, not done yet

--- build_database.sh
+++ build_database.sh
@@ -5,4 +5,10 @@ perl break.pl <scores-sorted.txt >scores-final.txt
 perl break.pl <pterms-sorted.txt >pterms-final.txt
 perl break.pl <rterms-sorted.txt >rterms-final.txt
 perl break.pl <reviews.txt >reviews-final.txt
-#db_load
+
+if [ -n "$1" ]; then 
+    echo "Database given: $1"
+    db_load
+
+else echo "No database argument given."
+fi
