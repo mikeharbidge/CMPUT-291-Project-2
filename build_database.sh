bb733ed9a7c61b742b9120feda5211f2c75f43fd
mikeharbidge <55110294+mikeharbidge@users.noreply.github.com>
2020-04-02 14:45:14 -0600

    Added dupsort

--- build_database.sh
+++ build_database.sh
@@ -6,7 +6,7 @@ perl break.pl <pterms-sorted.txt >pterms-final.txt
 perl break.pl <rterms-sorted.txt >rterms-final.txt
 perl break.pl <reviews.txt >reviews-final.txt
 
-db_load -c duplicates=1 -f scores-final.txt -T -t btree sc.idx
-db_load -c duplicates=1 -T -t btree -f pterms-final.txt pt.idx
-db_load -c duplicates=1 -T -t btree -f rterms-final.txt rt.idx
+db_load -c duplicates=1 -c dupsort=1 -f scores-final.txt -T -t btree sc.idx
+db_load -c duplicates=1 -c dupsort=1 -T -t btree -f pterms-final.txt pt.idx
+db_load -c duplicates=1 -c dupsort=1 -T -t btree -f rterms-final.txt rt.idx
 db_load -T -t hash -f reviews-final.txt rw.idx
